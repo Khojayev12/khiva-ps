9f48ed8e8b5ce2ce0ba9b139827a7b308dc0aa7c
Xo'jayev Yo'ldoshbek <khojayev12@.com>
2023-04-17 21:14:57 +0500

    animation fixed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,6 +4,7 @@ import Logo from "../media/Logo.svg";
 import LangPanel from "./LangPanel";
 import { LangContext } from "./LangContext";
 import { Link, useLocation } from "react-router-dom";
+import { HiMenu } from "react-icons/hi";
 
 export default function Header(props) {
   const myRouter = useLocation();
@@ -55,7 +56,54 @@ export default function Header(props) {
               <LangPanel setLang={props.setLang} />
             </div>
           </div>
-          <div className="mobile-header"></div>
+          <div className="mobile-header">
+            <nav role="navigation">
+              <div id="menuToggle">
+                <input type="checkbox" />
+                <span></span>
+                <span></span>
+                <span></span>
+                <ul id="menu">
+                  <div className="single-menu">
+                    <Link to="/" className="my-link">
+                      {lang.mainPage}
+                    </Link>
+                  </div>
+                  <div className="single-menu">
+                    <Link to="/news" className="my-link">
+                      {lang.yangiliklar}
+                    </Link>
+                  </div>
+                  <div className="single-menu">
+                    <Link to="/galereya" className="my-link">
+                      {lang.galereya}
+                    </Link>
+                  </div>
+                  <div className="single-menu">
+                    <Link to="/education" className="my-link">
+                      {lang.education}
+                    </Link>
+                  </div>
+                  <div className="single-menu">
+                    <Link to="/about" className="my-link">
+                      {lang.aboutSchool}
+                    </Link>
+                  </div>
+                  <div
+                    className="single-menu"
+                    onClick={() => {
+                      window.scrollTo({
+                        top: document.body.scrollHeight,
+                        behavior: "smooth",
+                      });
+                    }}
+                  >
+                    {lang.contact}
+                  </div>
+                </ul>
+              </div>
+            </nav>
+          </div>
         </>
       )}
     </LangContext.Consumer>
